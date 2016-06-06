215579142befcc66eb178640b9afe84c7301c336
edsonc <bashpythonstuff@hotmail.co.uk>
2016-06-06 16:55:13 +0100

    add condition for file paths. Add log file

--- proxy_gen.py
+++ proxy_gen.py
@@ -17,6 +17,7 @@ for more information:
 
 """
 
+import logging
 import os
 import sys
 import subprocess
@@ -27,12 +28,30 @@ CRF_VALUE = '35'
 VIDEO_BR = '100' + 'k'
 AUDIO_BR = '96' + 'k'
 PRESET = 'ultrafast'
-FILETYPES = (".mov", ".mxf")
+FILETYPES = (".mov", ".mxf", ".mpg", ".avi")
+
+# Set up logging
+fflog = logging.getLogger(__name__)
+fflog.setLevel(logging.DEBUG)
+
+fhandler = logging.FileHandler('proxy_gen.log')
+#fhandler.setLevel(logging,DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fhandler.setFormatter(formatter)
+
+fflog.addHandler(fhandler)
 
 
+# Main application
 if len(sys.argv) > 1:
     target_dir = sys.argv[1]
     dest_dir = sys.argv[2]
+    if target_dir[-1] == '/':
+        target_dir = target_dir[:-1]
+    if dest_dir[-1] == '/':
+        dest_dir = dest_dir[:-1]
+    fflog.info('{}\n{}'.format(target_dir, dest_dir))
 else:
     print('Please provide an input and output directory')
 
@@ -49,16 +68,22 @@ def scan_files(target):
     for root, dirs, files in os.walk(target):
         for d in dirs:
             if not os.path.exists(d):
-                proxy_directory = dest_dir + target_dir + d
+                logging.info('Directory name: {}'.format(d))
+                # proxy_directory = dest_dir + target_dir + d
+                proxy_directory = dest_dir + target_dir + '/' + d
                 try:
                     os.makedirs(proxy_directory)
                 except OSError:
-                    print('Directory exists')
+                    fflog.error('Directory exists: {}'.format(proxy_directory))
                     continue
         for f in files:
             if f.lower().endswith(FILETYPES):
                 mov_file = os.path.join(root, f)
+                proxy_file = dest_dir + mov_file.replace(
+                        os.path.splitext(mov_file)[1], '.mp4')
                 if '.AppleDouble' in mov_file or '/._' in mov_file:
+                    continue
+                if os.path.isfile(proxy_file):
                     continue
                 else:
                     build_proxy(mov_file)
