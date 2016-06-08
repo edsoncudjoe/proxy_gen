0ea9cebb02e6fc68ecb3c0a9c92a1ea9ef188fdf
edsonc <bashpythonstuff@hotmail.co.uk>
2016-06-08 10:46:41 +0100

    change how os.walk scans directories

--- proxy_gen.py
+++ proxy_gen.py
@@ -1,7 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 """
-This python script uses ffmpeg to make proxy files from high
-resolution Apple ProRes mov files.
+This python script uses ffmpeg to make proxy files from higher
+resolution video files.
 
 Input 1: Target directory containing the media.
 Input 2: Destination directory for proxy files.
@@ -35,7 +35,6 @@ fflog = logging.getLogger(__name__)
 fflog.setLevel(logging.DEBUG)
 
 fhandler = logging.FileHandler('proxy_gen.log')
-#fhandler.setLevel(logging,DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fhandler.setFormatter(formatter)
@@ -43,7 +42,7 @@ fhandler.setFormatter(formatter)
 fflog.addHandler(fhandler)
 
 
-# Main application
+# Check user input arguments
 if len(sys.argv) > 1:
     target_dir = sys.argv[1]
     dest_dir = sys.argv[2]
@@ -60,17 +59,16 @@ def scan_files(target):
     """
     Checks if proxy destination already exists and creates
     the folder if not.
-    Searches for mov and mxf videos to create proxy files.
+    Searches for videos to create proxy files from.
     """
     destination = dest_dir + target_dir
     if not os.path.exists(destination):
         os.makedirs(destination)
     for root, dirs, files in os.walk(target):
-        for d in dirs:
+        for d in dirs:            
             if not os.path.exists(d):
                 logging.info('Directory name: {}'.format(d))
-                # proxy_directory = dest_dir + target_dir + d
-                proxy_directory = dest_dir + target_dir + '/' + d
+                proxy_directory = dest_dir + os.path.join(root, d)
                 try:
                     os.makedirs(proxy_directory)
                 except OSError:
@@ -92,6 +90,7 @@ def scan_files(target):
 
 
 def build_proxy(fname):
+    outfile = fname.replace(os.path.splitext(fname)[1], '.mp4')
     try:
         command = [
             FFMPEG_PATH, '-i', fname,
@@ -107,12 +106,13 @@ def build_proxy(fname):
             '-c:a', 'aac',
             '-ac', '2',
             '-b:a', AUDIO_BR,
-            '{}{}.mp4'.format(dest_dir, fname.replace(os.path.splitext(fname)[1], ''))
+            '{}{}'.format(dest_dir, outfile)
             ]
+        print('\nBuilding proxy file: {}'.format(outfile))
         subprocess.call(command)
     except Exception:
         print(Exception)
-
+        logging.error('{} : {}'.format(Exception, outfile))
 
 if __name__ == '__main__':
     scan_files(target_dir)
